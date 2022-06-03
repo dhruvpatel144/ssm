98273aa90b0ebc00198023ceec647b07cb9e0f2a
dhruvpatel144 <76422222+dhruvpatel144@users.noreply.github.com>
2022-06-03 13:46:52 +0530

    Update functions.py

--- ssm/functions.py
+++ ssm/functions.py
@@ -1,18 +1,18 @@
-def next_state(t,x,k):
+def next_state(A,t,x,k):
   a=x
   for h in range(k):
     a-=A[h,t]
     if(a<0):
       return h
 
-def out(st,y,p):
+def out(B,st,y,p):
   a=y
   for u in range(p):
     a-=B[st,u]
     if(a<0):
       return u
 
-def first_state(pi, x, y,k):
+def first_state(pi, x ,k):
   a= x[0]
   for h in range(k):
     a-=pi[h]
@@ -22,13 +22,13 @@ def first_state(pi, x, y,k):
 
 def hmm(A,B,pi,x,y,sample,outcome,p,k,l):
 
-  z1 = first_state(pi,x,y,k)
+  z1 = first_state(pi,x,k)
   sample = sample.at[0].set(z1)
   outcome = outcome.at[0].set(out(z1,y[0],p))
   z_i= z1
   for j in range(1,l):
-    z_i1=next_state(z_i,x[j],k)
+    z_i1=next_state(A,z_i,x[j],k)
     sample = sample.at[j].set(z_i1)
-    outcome = outcome.at[j].set(out(z_i1,y[j],p))
+    outcome = outcome.at[j].set(out(B,z_i1,y[j],p))
     z_i=z_i1
   return sample, outcome
