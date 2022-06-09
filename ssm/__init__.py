ff8370955a7f0aeea9ca09fb632ba50d115b6b7f
dhruvpatel144 <76422222+dhruvpatel144@users.noreply.github.com>
2022-06-09 15:38:13 +0530

    Update __init__.py

--- ssm/__init__.py
+++ ssm/__init__.py
@@ -1,3 +1,3 @@
 from ._version import version as __version__
 from .functions import first_state, next_state, out, hmm
-from .functions import discreteHMM, continuousHMM
+from .mainfunction import discreteHMM, continuousHMM
